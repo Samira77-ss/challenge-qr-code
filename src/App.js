70c04d81f6e169b4e8b1eeef5814088d61fae6c4
Samira77-ss <samiraseyfii0709@gmail.com>
2023-10-13 19:28:16 +0330

    Added changes to the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="qr-code">
+          <img
+            src="https://play-lh.googleusercontent.com/sQudn3Wh5Ffigo3gfnBnY-jWwL1KfqQSW7aKGTL_PIdUGnPcV29SoyKKsnHYHNlAp5k"
+            alt="QRCODE"
+          />
+          <h1>Improve your front-end skills by building projects</h1>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+      <footer>
+        This page was coded by Samira Seyfi{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Samira77-ss/challenge-product"
           target="_blank"
-          rel="noopener noreferrer"
+          alt="GithHub Profile"
+          rel="noreferrer"
         >
-          Learn React
+          open-source-code on GitHub
         </a>
-      </header>
+        , and hosted on{" "}
+        <a
+          href="https://tranquil-klepon-bd88fd.netlify.app"
+          alt="Netlify link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
